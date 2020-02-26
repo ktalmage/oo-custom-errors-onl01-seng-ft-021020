024d4d99447eca63867fb7d7eb6d2c526fe8cbce
Kyle Talmage <ktalmage85@gmail.com>
2020-02-26 22:30:32 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,10 +7,19 @@ class Person
 
   def get_married(person)
     self.partner = person
+    if person.class != Person
     person.partner = self
+    else
+      person.partner = self
+    end
   end
 
 end
+
+  class PartnerError < Standard Error
+  end
+end
+
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
